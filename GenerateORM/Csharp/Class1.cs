a4fc3d5b3db339bb853b336278b379b405763ec1
looping42 <loopingr42@gmail.com>
2017-04-14 19:35:28 +0200

    Second Commint Install en cours

---
+++ GenerateORM/Csharp/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace GenerateORM.Csharp
+{
+    internal class Contact
+    {
+        //public int Id { get; set; }
+        //public string FirstName { get; set; }
+        //public string LastName { get; set; }
+        //public DateTime? DateOfBirth { get; set; }
+        //public string PhoneNumber { get; set; }
+
+        //public static void test()
+        //{
+        //    using (SqlConnection connection = new SqlConnection(connectionString))
+        //    {
+        //        // code qui utilise la connexion
+        //        IEnumerable<Contact> contacts = connection.Query<Contact>("select * from Contact");
+        //    }
+        //}
+    }
+}
