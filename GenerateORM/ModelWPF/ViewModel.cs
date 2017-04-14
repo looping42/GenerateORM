8913936001e113cddc617e1b122b05ce6abb88a7
looping42 <loopingr42@gmail.com>
2017-04-14 22:32:34 +0200

    Commit test

---
+++ GenerateORM/ModelWPF/ViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateORM.ModelWPF
+{
+    internal class ViewModel
+    {
+        //private Bdd obj = new Bdd();
+
+        //public string TxtCustomerName
+        //{
+        //    get { return obj.CustomerName; }
+        //    set { obj.CustomerName = value; }
+        //}
+
+        //public string TxtAmount
+        //{
+        //    get { return Convert.ToString(obj.Amount); }
+        //    set { obj.Amount = Convert.ToDouble(value); }
+        //}
+
+        //public string LblAmountColor
+        //{
+        //    get
+        //    {
+        //        if (obj.Amount > 2000)
+        //        {
+        //            return "Blue";
+        //        }
+        //        else if (obj.Amount > 1500)
+        //        {
+        //            return "Red";
+        //        }
+        //        return "Yellow";
+        //    }
+        //}
+
+        //public bool IsMarried
+        //{
+        //    get
+        //    {
+        //        if (obj.Married == "Married")
+        //        {
+        //            return true;
+        //        }
+        //        else
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //}
+    }
+}
